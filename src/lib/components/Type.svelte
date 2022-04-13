94b747c40c5a012e94074b8244539e541cb0861f
development-vargamarcel <development.vargamarcel@gmail.com>
2022-04-13 11:14:43 +0200

    'generic'

--- src/lib/components/Type.svelte
+++ src/lib/components/Type.svelte
@@ -4,6 +4,22 @@
 
 	export let index;
 	export let type;
+	let name = type?.type?.ofType?.name || type?.type?.name || type?.name;
+	let nameToDisplay = type?.name || type?.type?.name || type?.type?.ofType?.name;
+	let kinds = [];
+
+	if (type?.kind) {
+		kinds.push(type?.kind);
+	}
+	if (type?.type?.kind) {
+		kinds.push(type?.type?.kind);
+	}
+	if (type?.ofType?.kind) {
+		kinds.push(type?.ofType?.kind);
+	}
+	if (type?.type?.ofType?.kind) {
+		kinds.push(type?.type?.ofType?.kind);
+	}
 	const rootTypeByName = (name) => {
 		return $introspectionResult.rootTypes.filter((item) => {
 			return item.name == name;
@@ -21,30 +37,42 @@
 	};
 	let showExpand = false;
 	let expandData = {};
+	let canExpand = false;
+	if (!kinds.includes('SCALAR')) {
+		canExpand = true;
+	}
 	const expand = () => {
 		showExpand = true;
-		expandData = rootTypeByName(type.name);
-		console.log(expandData.fields);
+		expandData = rootTypeByName(name);
+		console.log(expandData);
 	};
-	let whatIsShown = `expanded ${type.name}`;
 </script>
 
 <li class="my-1 p-1 bg-base-300 rounded  space-x-2 py-2 normal-case text-xs">
 	<div class="flex space-x-2">
-		<div class="bg-secondary p-1 rounded">{index}</div>
-		<div class="bg-secondary p-1 rounded">{type.kind}</div>
-		<div
-			class="btn btn-xs btn-info normal-case font-light"
-			on:click={() => {
-				console.log(type);
-			}}
-		>
-			{type.name}
+		<div class="flex space-x-2 w-1/3">
+			<div class="bg-secondary p-1 rounded">{index}</div>
+			<div class="bg-secondary p-1 rounded ">{kinds.join(' of ')}</div>
 		</div>
-		<div class="btn btn-xs  p-1 rounded" on:click={expand}>expand</div>
+
+		<div class="w-1/2">
+			<div
+				class="btn btn-xs btn-info normal-case font-light "
+				on:click={() => {
+					console.log(type);
+				}}
+			>
+				{nameToDisplay}
+			</div>
+		</div>
+		<div class="w-1/8">
+			{#if canExpand}
+				<div class="btn btn-xs  p-1 rounded" on:click={expand}>expand</div>
+			{/if}
+		</div>
 	</div>
 
 	{#if showExpand}
-		<Types whatToShow={expandData.fields} {whatIsShown} />
+		<Types whatToShow={expandData.fields} whatIsShown="" />
 	{/if}
 </li>
--- src/lib/components/Types.svelte
+++ src/lib/components/Types.svelte
@@ -2,12 +2,19 @@
 	import Type from './Type.svelte';
 
 	export let whatIsShown;
-	export let whatToShow;
+	export let whatToShow = '';
 </script>
 
-<h1 class="py-2">showing {whatIsShown}</h1>
-<ul>
-	{#each whatToShow as type, index}
-		<Type {index} {type} />
-	{/each}
-</ul>
+<div class="border-l-2">
+	{#if whatIsShown !== ''}
+		<h1 class="py-1 my-1 px-2 bg-base-content rounded-r-full text-base-300 text-center">
+			showing {whatIsShown}
+		</h1>
+	{/if}
+
+	<ul>
+		{#each whatToShow as type, index (index + whatIsShown)}
+			<Type {index} {type} />
+		{/each}
+	</ul>
+</div>
