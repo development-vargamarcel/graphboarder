e14043ebe6794aa7b3d1fbfe0c276c0be974b71e
development-vargamarcel <development.vargamarcel@gmail.com>
2022-04-13 20:16:37 +0200

    'generic'

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapterNetlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +13,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapterNetlify(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
