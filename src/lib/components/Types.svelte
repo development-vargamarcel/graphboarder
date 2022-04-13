e6de614b1016c2f65c5012a566ddec9e9468b8a0
development-vargamarcel <development.vargamarcel@gmail.com>
2022-04-13 22:54:24 +0200

    'generic'

--- src/lib/components/Types.svelte
+++ src/lib/components/Types.svelte
@@ -7,7 +7,9 @@
 
 <div class="border-l-2">
 	{#if whatIsShown !== ''}
-		<h1 class="py-1 my-1 px-2 bg-base-content rounded-r-full text-base-300 text-center">
+		<h1
+			class="py-1 my-1 px-2 bg-base-content rounded-r-full text-base-300 text-center text-md w-1/2"
+		>
 			showing {whatIsShown}
 		</h1>
 	{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,91 +3,103 @@
 </script>
 
 <script lang="ts">
-	// import { introspectionResult } from '$lib/stores/introspectionResult';
-	// console.log($introspectionResult);
-	// import Types from '$lib/components/Types.svelte';
+	import { introspectionResult } from '$lib/stores/introspectionResult';
+	console.log($introspectionResult);
+	import Types from '$lib/components/Types.svelte';
 
-	// let rootTypes = $introspectionResult.rootTypes;
-	// let queries = $introspectionResult.queryFields;
-	// let mutations = $introspectionResult.mutationFields;
-	// let whatIsShown = '-';
-	// let whatToShow = [];
-	// let sortingInputValue = '';
-	// let sortingArray = [];
-	// $: sortingArray = sortingInputValue.split(' ');
+	let rootTypes = $introspectionResult.rootTypes;
+	let queries = $introspectionResult.queryFields;
+	let mutations = $introspectionResult.mutationFields;
+	let whatIsShown = '-';
+	let whatToShow = [];
+	let sortingInputValue = '';
+	let sortingArray = [];
+	$: sortingArray = sortingInputValue.split(' ');
 
-	// const filterByWord = () => {
-	// 	rootTypes = rootTypes.filter((type) => {
-	// 		if (sortingArray.length == 1 && sortingArray[0] == '') {
-	// 			return true;
-	// 		} else {
-	// 			return sortingArray.includes(type.kind);
-	// 		}
-	// 	});
+	const filterByWord = () => {
+		rootTypes = rootTypes.filter((type) => {
+			if (sortingArray.length == 1 && sortingArray[0] == '') {
+				return true;
+			} else {
+				return sortingArray.includes(type.kind);
+			}
+		});
 
-	// 	showRootTypes();
-	// };
-	// const showRootTypes = () => {
-	// 	console.log(rootTypes);
-	// 	whatToShow = rootTypes;
-	// 	whatIsShown = 'rootTypes';
-	// };
-	// showRootTypes();
-	// const showQueries = () => {
-	// 	console.log(queries);
-	// 	whatToShow = queries;
-	// 	whatIsShown = 'queryFields';
-	// };
+		showRootTypes();
+	};
+	const showRootTypes = () => {
+		console.log(rootTypes);
+		whatToShow = rootTypes;
+		whatIsShown = 'rootTypes';
+	};
+	showRootTypes();
+	const showQueries = () => {
+		if (queries) {
+			console.log(queries);
+			whatToShow = queries;
+		} else {
+			whatToShow = [];
+		}
+		whatIsShown = 'queryFields';
+	};
 
-	// const showMutations = () => {
-	// 	console.log(mutations);
-	// 	whatToShow = mutations;
-	// 	whatIsShown = 'mutationFields';
-	// };
+	const showMutations = () => {
+		if (mutations) {
+			console.log(mutations);
+			whatToShow = mutations;
+		} else {
+			whatToShow = [];
+		}
+		whatIsShown = 'mutationFields';
+	};
 
-	// console.log($introspectionResult);
-	// const rootTypeByName = (name) => {
-	// 	return $introspectionResult.rootTypes.filter((item) => {
-	// 		return item.name == name;
-	// 	})[0];
-	// };
-	// const queryFieldByName = (name) => {
-	// 	return $introspectionResult.queryFields.filter((item) => {
-	// 		return item.name == name;
-	// 	})[0];
-	// };
-	// const mutationFieldByName = (name) => {
-	// 	return $introspectionResult.mutationFields.filter((item) => {
-	// 		return item.name == name;
-	// 	})[0];
-	// };
+	console.log($introspectionResult);
+	const rootTypeByName = (name) => {
+		return $introspectionResult.rootTypes.filter((item) => {
+			return item.name == name;
+		})[0];
+	};
+	const queryFieldByName = (name) => {
+		return $introspectionResult.queryFields.filter((item) => {
+			return item.name == name;
+		})[0];
+	};
+	const mutationFieldByName = (name) => {
+		return $introspectionResult.mutationFields.filter((item) => {
+			return item.name == name;
+		})[0];
+	};
+	let auth_token = '';
+	const store_auth_token = () => {
+		localStorage.setItem('auth_token', auth_token);
+	};
 </script>
 
-<!-- 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
 <section class="">
+	<input type="text" class="input input-sm" bind:value={auth_token} />
+	<button class="btn bg-primary btn-sm" on:click={store_auth_token}>store_auth_token</button>
+	<br />
+	<br />
 	<button
-		class="btn btn-primary"
+		class="btn btn-primary btn-sm"
 		on:click={() => {
 			console.log(rootTypeByName('articles'));
 		}}>rootTypes_by_name</button
 	>
+	<input type="text" class="input input-sm" bind:value={sortingInputValue} />
+	<button class="btn bg-primary btn-sm" on:click={filterByWord}>filter</button>
 	<br />
-
+	<br />
 	<button class="btn" on:click={showRootTypes}>show all</button>
 	<button class="btn" on:click={showQueries}>show queries</button>
 	<button class="btn" on:click={showMutations}>show mutations</button>
 
-	<br />
-	<input type="text" class="input input-sm" bind:value={sortingInputValue} />
-	<button class="btn bg-primary" on:click={filterByWord}>filter</button>
-	<br />
-	<br />
-
 	<Types {whatToShow} {whatIsShown} />
-</section> -->
+</section>
+
 <style>
 </style>
