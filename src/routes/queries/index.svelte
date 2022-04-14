56710ed77e4f60a2d7df5a0e54a786f589804b17
development-vargamarcel <development.vargamarcel@gmail.com>
2022-04-14 08:46:52 +0200

    'generic'

--- src/routes/queries/index.svelte
+++ src/routes/queries/index.svelte
@@ -10,9 +10,14 @@
 	};
 </script>
 
-<div class="w-full  my-4 pl-12">
+<div class="w-full  my-4 px-4">
 	<div class="flex w-1/4  space-x-2">
-		<input type="text" class="input input-sm " placeholder="jwt token" bind:value={auth_token} />
+		<input
+			type="text"
+			class="input input-sm w-40"
+			placeholder="jwt token"
+			bind:value={auth_token}
+		/>
 		<button class="btn bg-primary btn-sm normal-case w-content-min" on:click={store_auth_token}
 			>store auth_token</button
 		>
