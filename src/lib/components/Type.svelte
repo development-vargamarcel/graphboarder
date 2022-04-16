b962668e276a9cd1a88461dd4401accc10ab09d2
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-04-16 08:41:13 +0200

    Update Type.svelte

--- src/lib/components/Type.svelte
+++ src/lib/components/Type.svelte
@@ -130,8 +130,8 @@
 </script>
  
 <li 
-	class="my-4 bg-primary/5 pb-0 pl-1 pr-0  rounded-r-sm rounded-l-none shadow-none  space-x-2  normal-case text-xs {showExpand
-		? 'mb-2/'
+	class="mb-4 bg-primary/5 pb-0 pl-1 pr-0  rounded-r-sm rounded-l-none shadow-none  space-x-2  normal-case text-xs {showExpand
+		? ''
 		: ''}"
 >
 	<div class="flex space-x-2">
