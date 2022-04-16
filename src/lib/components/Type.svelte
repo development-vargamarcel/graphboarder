13d50429af004a200021c33641be77fc11e3cd3b
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-04-16 09:41:51 +0200

    Update Type.svelte

--- src/lib/components/Type.svelte
+++ src/lib/components/Type.svelte
@@ -129,10 +129,10 @@
 	};
 </script>
  
-{#if !showExpand}
+
 <div class="pt-2 text-center text-xs">
 </div>
-{/if}
+
 
 <div
 	class="  pb-0 pl-1 pr-0  rounded-r-sm rounded-l-none shadow-none  space-x-2  normal-case text-xs {showExpand
