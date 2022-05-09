0a4e32f28a95145b11644fe1dfa2d6820b51d440
development-vargamarcel <development.vargamarcel@gmail.com>
2022-05-09 17:43:39 +0200

    'generic'

--- src/lib/components/ActiveArguments.svelte
+++ src/lib/components/ActiveArguments.svelte
@@ -13,29 +13,45 @@
 			{#if activeArgumentData.displayType == 'ENUM'}
 				<div class="flex flex-col">
 					{#if activeArgumentData.expectsList}
-						{#each activeArgumentData.enumValues as enumValue}
-							<label class="label" name={enumValue.name}>
-								{enumValue.name}
-								<input type="checkbox" class="checkbox input-primary" />
-							</label>
-						{/each}
+						<div class="dropdown">
+							<label tabindex="0" class="btn m-1">choose</label>
+							<div
+								tabindex="0"
+								class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+							>
+								{#each activeArgumentData.enumValues as enumValue}
+									<label class="label" name={enumValue.name}>
+										{enumValue.name}
+										<input type="checkbox" class="checkbox input-primary" />
+									</label>
+								{/each}
+							</div>
+						</div>
 					{:else}
-						{#each activeArgumentData.enumValues as enumValue}
-							<label class="label">
-								{enumValue.name}
-								<input
-									type="radio"
-									class="radio input-primary"
-									name={activeArgumentData.stepsOfFieldsNewStringified}
-								/>
-							</label>
-						{/each}
+						<div class="dropdown">
+							<label tabindex="0" class="btn m-1">choose</label>
+							<div
+								tabindex="0"
+								class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+							>
+								{#each activeArgumentData.enumValues as enumValue}
+									<label class="label">
+										{enumValue.name}
+										<input
+											type="radio"
+											class="radio input-primary"
+											name={activeArgumentData.stepsOfFieldsNewStringified}
+										/>
+									</label>
+								{/each}
+							</div>
+						</div>
 					{/if}
 				</div>
 			{:else if activeArgumentData.displayType == 'INPUT_OBJECT'}
 				<div class="flex flex-col">
 					<div class="dropdown">
-						<label tabindex="0" class="btn m-1">{activeArgumentData.inputFields[0].name}</label>
+						<label tabindex="0" class="btn m-1">choose and fill</label>
 						<div
 							tabindex="0"
 							class="dropdown-content  p-2 shadow bg-base-100 rounded-box max-w-min"
