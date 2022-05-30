aa719020c625893eb93da5bf3388ffec8a0dad72
development-vargamarcel <development.vargamarcel@gmail.com>
2022-05-30 13:47:44 +0200

    'generic'

--- src/lib/components/fields/Map.svelte
+++ src/lib/components/fields/Map.svelte
@@ -1,23 +1,24 @@
 <script>
 	import * as mapboxglOriginal from 'mapbox-gl';
 	import { onMount } from 'svelte';
-    let mapboxgl=mapboxglOriginal
+	let mapboxgl = mapboxglOriginal;
+	let map;
 	onMount(() => {
-		mapboxgl.accessToken ='pk.eyJ1IjoiZGV2ZWxvcG1lbnQtdmFyZ2FtYXJjZWwiLCJhIjoiY2wwZTQ1bno0MDEzZjNvb2I3MHZydTR4dyJ9.EyYgnR9m4Efkd3T-NyrFjA';
-		const map = new mapboxgl.Map({
+		mapboxgl.accessToken =
+			'pk.eyJ1IjoiZGV2ZWxvcG1lbnQtdmFyZ2FtYXJjZWwiLCJhIjoiY2wwZTQ1bno0MDEzZjNvb2I3MHZydTR4dyJ9.EyYgnR9m4Efkd3T-NyrFjA';
+		map = new mapboxgl.Map({
 			container: 'map', // container ID
 			style: 'mapbox://styles/mapbox/streets-v11', // style URL
 			center: [-74.5, 40], // starting position [lng, lat]
 			zoom: 9 // starting zoom
 		});
 	});
-	let large = false;
+	let showMap = true;
 </script>
 
 <div class="flex flex-col">
-	<button on:click="">large/small</button>
-	<div class="bg-black  w-20 h-20">
-		<div id="map" class="h-20 w-20 absolute" />
+	<div class="">
+		<div id="map" class="w-32 h-32  " />
 	</div>
 </div>
 
