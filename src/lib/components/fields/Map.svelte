60748895b437dca88a8dc38ea67ec5c2911ff05f
development-vargamarcel <development.vargamarcel@gmail.com>
2022-05-30 13:19:29 +0200

    'generic'

--- src/lib/components/fields/Map.svelte
+++ src/lib/components/fields/Map.svelte
@@ -1,9 +1,9 @@
 <script>
-	import * as mapboxgl from 'mapbox-gl';
+	import * as mapboxglOriginal from 'mapbox-gl';
 	import { onMount } from 'svelte';
+    let mapboxgl=mapboxglOriginal
 	onMount(() => {
-		mapboxgl.accessToken =
-			'pk.eyJ1IjoiZGV2ZWxvcG1lbnQtdmFyZ2FtYXJjZWwiLCJhIjoiY2wwZTQ1bno0MDEzZjNvb2I3MHZydTR4dyJ9.EyYgnR9m4Efkd3T-NyrFjA';
+		mapboxgl.accessToken ='pk.eyJ1IjoiZGV2ZWxvcG1lbnQtdmFyZ2FtYXJjZWwiLCJhIjoiY2wwZTQ1bno0MDEzZjNvb2I3MHZydTR4dyJ9.EyYgnR9m4Efkd3T-NyrFjA';
 		const map = new mapboxgl.Map({
 			container: 'map', // container ID
 			style: 'mapbox://styles/mapbox/streets-v11', // style URL
@@ -15,7 +15,7 @@
 </script>
 
 <div class="flex flex-col">
-	<button>large/small</button>
+	<button on:click="">large/small</button>
 	<div class="bg-black  w-20 h-20">
 		<div id="map" class="h-20 w-20 absolute" />
 	</div>
