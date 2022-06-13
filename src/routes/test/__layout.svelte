4acfc299d3fdc5b74aca8ca9e6042dd1d6e2759d
development-vargamarcel <development.vargamarcel@gmail.com>
2022-06-13 12:21:05 +0200

    'generic'

---
+++ src/routes/test/__layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import List from './../../lib/components/List.svelte';
+
+	let items1 = [
+		{ id: 1, title: 'I' },
+		{ id: 2, title: 'Am' },
+		{ id: 3, title: 'Yoda' },
+		{
+			id: 5654,
+			filterOperators: [
+				{
+					operator: '_and',
+					items: [
+						{ id: 12, title: 'fdsf' },
+						{ id: 13, title: 'fdsfds' },
+						{ id: 14, title: 'fdsdf' }
+					]
+				}
+			]
+		}
+	];
+</script>
+
+<h3>You can't mix the light side and the dark side</h3>
+<hr />
+<List items={items1} type="light" />
