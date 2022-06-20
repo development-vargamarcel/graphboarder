7bfdc6f9931630ab081d2b2f02c29a285e7e2c77
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-06-20 10:26:58 +0200

    Update ActiveArgumentGroupHasFilterOperators.svelte

--- src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
@@ -75,7 +75,7 @@ const dragDisabledConstantTest=true
 </script>
 
 <div
-	class=" w-full"
+	class=" w-full {node?.operator? 'bg-accent/25':''} "
 	bind:this={labelEl}
 	on:mousedown={() => {
 		dragDisabled = true;
