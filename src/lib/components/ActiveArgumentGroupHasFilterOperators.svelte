412144b25b5b240aa2742dfa492d1cfe3d1835db
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-06-20 13:02:41 +0200

    Update ActiveArgumentGroupHasFilterOperators.svelte

--- src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
@@ -75,7 +75,7 @@ const dragDisabledConstantTest=true
 </script>
 
 <div
-	class=" w-full {node?.operator? 'bg-warning/5':''} "
+	class=" w-full {node?.operator? 'bg-secondary/5':''} "
 	bind:this={labelEl}
 	on:mousedown={() => {
 		dragDisabled = true;
