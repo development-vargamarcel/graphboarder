4b088e44ce979cd43bde22212887a6f28d75c4d0
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-06-21 11:39:22 +0200

    Update ActiveArgumentGroupHasFilterOperators.svelte

--- src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
@@ -76,10 +76,10 @@
 
 <div
 	class=" w-full transition-all duration-500 my-1  {node?.operator
-		? 'rounded-l-md bg-gradient-to-br   border-l-[1px] ring ring-4  '
+		? 'rounded-l-md bg-gradient-to-br   border-l-[1px] ring ring-2 ring-offset-[1px]'
 		: 'pr-2'} 
 {node?.operator && node?.not
-				? ' ring-error/10'
+				? ' ring-error/25'
 				: 'ring-error/0'} 
 {node?.operator == '_and' ? 'border-primary from-primary-focus/10' : 'border-accent-focus from-accent-focus/10'}"
 on:contextmenu|preventDefault|stopPropagation={() => {
@@ -120,8 +120,8 @@ on:contextmenu|preventDefault|stopPropagation={() => {
 		<br />
 	{:else}
 		<div
-			class="transition-color duration-500 rounded-box ring ring-4   {node?.not
-				? ' ring-error/10'
+			class="transition-color duration-500 rounded-box ring  ring-2 ring-offset-[1px]   {node?.not
+				? ' ring-error/25'
 				: 'ring-error/0'}  w-full"
 		>
 			<ActiveArgument
