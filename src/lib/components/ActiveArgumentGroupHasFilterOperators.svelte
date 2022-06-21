0179f1ec7e99755903b874367d43af9a37be22f0
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-06-21 11:32:09 +0200

    Update ActiveArgumentGroupHasFilterOperators.svelte

--- src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
@@ -76,7 +76,7 @@
 
 <div
 	class=" w-full transition-all duration-500 my-1  {node?.operator
-		? 'rounded-l-md bg-gradient-to-br   border-l-[1px] ring ring-2  '
+		? 'rounded-l-md bg-gradient-to-br   border-l-[1px] ring ring-4  '
 		: 'pr-2'} 
 {node?.operator && node?.not
 				? ' ring-error/10'
@@ -120,7 +120,7 @@ on:contextmenu|preventDefault|stopPropagation={() => {
 		<br />
 	{:else}
 		<div
-			class="transition-color duration-500 rounded-box ring ring-2   {node?.not
+			class="transition-color duration-500 rounded-box ring ring-4   {node?.not
 				? ' ring-error/10'
 				: 'ring-error/0'}  w-full"
 		>
