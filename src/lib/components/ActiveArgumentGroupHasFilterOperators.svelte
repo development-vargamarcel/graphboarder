f89afa615dcff8fd33970f36a03beb024c352153
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-06-21 09:29:11 +0200

    Update ActiveArgumentGroupHasFilterOperators.svelte

--- src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
@@ -75,8 +75,8 @@
 </script>
 
 <div
-	class=" w-full transition-all duration-500 my-1 ring ring-2 ring-warning/5  {node?.operator
-		? 'rounded-l-md bg-gradient-to-br   border-l-[1px] '
+	class=" w-full transition-all duration-500 my-1  {node?.operator
+		? 'rounded-l-md bg-gradient-to-br   border-l-[1px] ring ring-2 ring-primary/5 '
 		: ''} {node?.operator == '_and' ? 'border-primary from-primary-focus/10 ' : 'border-accent-focus from-accent-focus/10'}"
 	bind:this={labelEl}
 	on:mousedown={() => {
