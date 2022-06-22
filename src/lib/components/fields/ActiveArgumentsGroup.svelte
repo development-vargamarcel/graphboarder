aa17ac2fa3b36a3bd7a174c74fdc413340fab2ce
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-06-22 22:22:41 +0200

    Update ActiveArgumentsGroup.svelte

--- src/lib/components/fields/ActiveArgumentsGroup.svelte
+++ src/lib/components/fields/ActiveArgumentsGroup.svelte
@@ -273,7 +273,7 @@ not:false,
 </div>
 
 {#if hasGroup_argsNode}
-<div class='overflow-x-auto' >
+<div class='Overflowxauto max-h-screen' >
 	<ActiveArgumentGroupHasFilterOperators
 		node={group.group_argsNode.mainContainer}
 		{group}
