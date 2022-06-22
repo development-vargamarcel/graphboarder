1b5c0103cad8896155ea95a89d0ed3b6ddd9f567
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-06-22 09:46:39 +0200

    Update ActiveArgumentGroupHasFilterOperators.svelte

--- src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
@@ -170,7 +170,7 @@
 						<div
 							tabindex={dragDisabled ? 0 : -1}
 							aria-label="drag-handle"
-							class="bi bi-grip-vertical bg-black -px-1"
+							class="bi bi-grip-vertical -pr-2"
 							style={dragDisabled ? 'cursor: grab' : 'cursor: grabbing'}
 							on:mousedown={startDrag}
 							on:touchstart={startDrag}
