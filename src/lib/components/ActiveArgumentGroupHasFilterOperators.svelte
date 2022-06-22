aa8ac7323d78f2740fe3c78cf90e6b7833601e0d
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-06-22 11:42:44 +0200

    Update ActiveArgumentGroupHasFilterOperators.svelte

--- src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentGroupHasFilterOperators.svelte
@@ -81,7 +81,7 @@
 						<div
 							tabindex={dragDisabled ? 0 : -1}
 							aria-label="drag-handle"
-							class="bi bi-grip-vertical -mr-1 text-lg {node?.operator ==undefined? 'text-base-content' :(node?.operator ==
+							class=" transition:all duration-500 bi bi-grip-vertical -mr-1 text-lg {node?.operator ==undefined? 'text-base-content' :(node?.operator ==
 			'_and'
 				? 'text-primary'
 				: 'text-accent-focus')}"
