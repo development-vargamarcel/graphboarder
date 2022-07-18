90eccd4a8f183985da6b4446cbd27c3486aede49
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2022-07-19 00:02:23 +0200

    Update TabItem.svelte

--- src/lib/components/TabItem.svelte
+++ src/lib/components/TabItem.svelte
@@ -6,12 +6,24 @@
 	export let icon;
 	export let url;
 	export let hasFill = true;
+const get_itemsToShow = () => {
+		let currentUrl = $page.url.pathname;
+		let currentUrlSplit = currentUrl.split('/');
+
+		console.log({ currentUrl });
+		return (itemsToShow =
+			links.filter((link) => {
+				let linkUrlSplit = link.url.split('/');
+				console.log(linkUrlSplit, currentUrlSplit);
+				return currentUrlSplit[1] == linkUrlSplit[1];
+			})[0]?.items ?? []);
+	};
 </script>
 
 <!-- <a
 	sveltekit:prefetch
 	href={url}
-	class="{$page.url.pathname === url
+	class="{$page.url.pathname?.split('/')?.[1] === url?.split('/')?.[1]
 		? ' font-medium scale-110  '
 		: ' opacity-50 '}  text-center text-base-content leading-none flex flex-col  mb-2 mt-1 justify-center  text-xs hover:no-underline focus:no-underline mx-auto border-b-4 border-base-content border-opacity-0 active:border-opacity-100 transform  cursor-pointer  ease-out duration-700"
 >	s</a> -->
