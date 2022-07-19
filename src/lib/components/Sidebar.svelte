3d373b9ecbfee2c7a724c275d5c986208b38ecb3
development-vargamarcel <development.vargamarcel@gmail.com>
2022-07-19 17:54:30 +0200

    'generic'

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -39,13 +39,16 @@
 	</div>
 </div>
 {#if forceVisibleSidebar}
-	<div class=" bg-black/50 fixed top-0 z-50">
-		<div
-			class="md:hidden "
-			in:fly={{ x: -300, duration: 600, opacity: 1 }}
-			out:fly={{ x: -350, duration: 300, opacity: 1 }}
-		>
-			<TabContainer />
-		</div>
+	<div
+		class=" bg-black/30 fixed top-0 z-50 md:hidden h-screen w-screen"
+		in:fade={{ duration: 600, opacity: 1 }}
+		out:fade={{ duration: 300, opacity: 1 }}
+	/>
+	<div
+		class="md:hidden fixed top-0 z-50"
+		in:fly={{ x: -300, duration: 600, opacity: 1 }}
+		out:fly={{ x: -350, duration: 300, opacity: 1 }}
+	>
+		<TabContainer />
 	</div>
 {/if}
