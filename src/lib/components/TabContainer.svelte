1226d948a022df92c0e2d67188cea89eb7ba92d0
development-vargamarcel <development.vargamarcel@gmail.com>
2022-07-22 22:53:42 +0200

    'generic'

--- src/lib/components/TabContainer.svelte
+++ src/lib/components/TabContainer.svelte
@@ -46,7 +46,7 @@
 	let itemsToShow = [];
 </script>
 
-<div class="flex h-screen pb-10">
+<div class="flex h-screen ">
 	<div class="w-16 ">
 		<div class="h-[50px] bg-primary ">
 			<a href="/" class="block w-full h-full ">
@@ -54,7 +54,7 @@
 			</a>
 		</div>
 		<ul
-			class="flex h-full w-16xxx flex-col  justify-start border-t-[1px] border-base-content border-opacity-5 bg-base-300   pt-1"
+			class="flex h-full w-16xxx flex-col  justify-start border-t-[1px] border-base-content border-opacity-5 bg-base-300   pt-1 pb-20"
 			on:click={() => {
 				// if (itemsToShow.length == 0) {
 				// 	dispatch('hideSidebar');
@@ -77,7 +77,7 @@
 		<div class="">
 			<div class="h-[50px] bg-accent">{''}</div>
 			<ul
-				class="space-y-1 px-4 py-4 h-full overflow-y-auto  w-[60vw] md:w-full   overflow-x-auto  bg-base-100  grow "
+				class="space-y-1 px-4 py-4 h-full overflow-y-auto  w-[60vw] md:w-full   overflow-x-auto  bg-base-100  grow pb-20"
 				on:click={() => {
 					dispatch('hideSidebar');
 				}}
