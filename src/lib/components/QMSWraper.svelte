d708413b09f8f08854ed11d6f60aecd8b84ecc46
development-vargamarcel <development.vargamarcel@gmail.com>
2022-11-08 13:15:21 +0100

    'generic'

--- src/lib/components/QMSWraper.svelte
+++ src/lib/components/QMSWraper.svelte
@@ -10,24 +10,24 @@
 	export let QMSName;
 
 	let activeArgumentsDataGrouped_Store = Create_activeArgumentsDataGrouped_Store();
-	setContext(`${prefix}activeArgumentsDataGrouped_Store`, activeArgumentsDataGrouped_Store);
 	const tableColsData_Store = Create_tableColsData_Store();
-	setContext(`${prefix}tableColsData_Store`, tableColsData_Store);
 	const final_gqlArgObj_Store = Create_final_gqlArgObj_Store(activeArgumentsDataGrouped_Store);
-	setContext(`${prefix}final_gqlArgObj_Store`, final_gqlArgObj_Store);
 	const QMS_body_Store = Create_QMS_body_Store(
 		tableColsData_Store,
 		activeArgumentsDataGrouped_Store,
 		QMSType,
 		QMSName
 	);
-	setContext(`${prefix}QMS_body_Store`, QMS_body_Store);
 	const QMS_body_StoreDerived = Create_QMS_body_StoreDerived(
 		final_gqlArgObj_Store,
 		tableColsData_Store,
 		QMSType,
 		QMSName
 	);
+	setContext(`${prefix}activeArgumentsDataGrouped_Store`, activeArgumentsDataGrouped_Store);
+	setContext(`${prefix}tableColsData_Store`, tableColsData_Store);
+	setContext(`${prefix}final_gqlArgObj_Store`, final_gqlArgObj_Store);
+	setContext(`${prefix}QMS_body_Store`, QMS_body_Store);
 	setContext(`${prefix}QMS_body_StoreDerived`, QMS_body_StoreDerived);
 </script>
 
