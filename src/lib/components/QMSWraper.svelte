ee88dcb6bbd95d69482b8fc2e14b60d7ee57294f
development-vargamarcel <development.vargamarcel@gmail.com>
2022-11-09 13:46:46 +0100

    'generic'

--- src/lib/components/QMSWraper.svelte
+++ src/lib/components/QMSWraper.svelte
@@ -21,7 +21,6 @@
 	setContext(`${prefix}activeArgumentsDataGrouped_Store`, activeArgumentsDataGrouped_Store);
 	setContext(`${prefix}tableColsData_Store`, tableColsData_Store);
 	setContext(`${prefix}final_gqlArgObj_Store`, final_gqlArgObj_Store);
-
 	setContext(`${prefix}QMS_body_StoreDerived`, QMS_body_StoreDerived);
 </script>
 
