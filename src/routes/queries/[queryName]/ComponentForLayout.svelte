2ace956a18ef3415eb1ab9daa067988ecb0edb9a
development-vargamarcel <development.vargamarcel@gmail.com>
2022-11-10 21:42:21 +0100

    'generic'

--- src/routes/queries/[queryName]/ComponentForLayout.svelte
+++ src/routes/queries/[queryName]/ComponentForLayout.svelte
@@ -183,12 +183,9 @@
 </div>
 
 <slot />
-
-{#if queryData.fetching}
-	<p>Loading...</p>
-{:else if queryData.error}
-	<div class="px-4 mx-auto">
-		<div class="alert alert-error shadow-lg">
+{#if queryData.error}
+	<div class="px-4 mx-auto  mb-2">
+		<div class="alert alert-error shadow-lg ">
 			<div>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -202,11 +199,15 @@
 						d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
 					/></svg
 				>
-				<span>{queryData.error}</span>
+				<span class="max-h-20 overflow-auto">{queryData.error}</span>
 			</div>
 		</div>
 	</div>
-{:else}
+{/if}
+{#if queryData.fetching}
+	<p>Loading...</p>
+{/if}
+{#if queryData.data}
 	<div class="md:px-2">
 		<Table
 			colsData={$tableColsData_Store}
