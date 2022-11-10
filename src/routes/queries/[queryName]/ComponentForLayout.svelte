1568478ea4f2f286d44bd36d8b54e62b8c5891fa
development-vargamarcel <development.vargamarcel@gmail.com>
2022-11-10 22:04:19 +0100

    'generic'

--- src/routes/queries/[queryName]/ComponentForLayout.svelte
+++ src/routes/queries/[queryName]/ComponentForLayout.svelte
@@ -187,18 +187,24 @@
 	<div class="px-4 mx-auto  mb-2">
 		<div class="alert alert-error shadow-lg ">
 			<div>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="stroke-current flex-shrink-0 h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-					/></svg
-				>
+				<button class="btn btn-ghost btn-sm p-0">
+					<svg
+						on:click={() => {
+							queryData.error = null;
+						}}
+						xmlns="http://www.w3.org/2000/svg"
+						class="stroke-current flex-shrink-0 h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+						/></svg
+					>
+				</button>
+
 				<span class="max-h-20 overflow-auto">{queryData.error}</span>
 			</div>
 		</div>
