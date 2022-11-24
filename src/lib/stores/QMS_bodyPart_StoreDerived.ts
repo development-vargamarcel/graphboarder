2bcd9edfb0c072e4a4701fae62a24c71cef26476
development-vargamarcel <development.vargamarcel@gmail.com>
2022-11-24 18:10:21 +0100

    'generic'

--- src/lib/stores/QMS_bodyPart_StoreDerived.ts
+++ src/lib/stores/QMS_bodyPart_StoreDerived.ts
@@ -1,13 +1,17 @@
 import { build_QMS_bodyPart, tableColsDataToQueryFields } from '$lib/utils/usefulFunctions';
 import { derived, get } from 'svelte/store'
+import { schemaData } from '$lib/stores/schemaData';
+
 
 export const Create_QMS_bodyPart_StoreDerived = (_final_grqlArgObj_Store, _tableColsData_Store, QMS_type, QMS_name, offsetBasedPaginationOptions_Store) => {
 
 
 
     return derived([_final_grqlArgObj_Store, _tableColsData_Store], ([$_final_grqlArgObj_Store, $_tableColsData_Store], set) => {
-        let currentOffset = $_final_grqlArgObj_Store?.final_gqlArgObj?.offset
-        if (get(offsetBasedPaginationOptions_Store)?.infiniteScroll) {
+        let QMS_Info = schemaData.get_QMS_Field(QMS_name, QMS_type);
+        let offsetName = QMS_Info?.offsetPaginationArgs.offsetArg.dd_displayName
+        let currentOffset = $_final_grqlArgObj_Store?.final_gqlArgObj?.[offsetName]
+        if (QMS_Info?.paginationType == "offsetBased" && get(offsetBasedPaginationOptions_Store)?.infiniteScroll) {
             let lastOffset
             console.log({ lastOffset })
             if (lastOffset == null || currentOffset != 0) {
