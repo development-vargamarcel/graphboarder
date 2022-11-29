0e33f8eb204e27a18511dc7387024afe371ae7ff
development-vargamarcel <development.vargamarcel@gmail.com>
2022-11-29 11:27:02 +0100

    'generic'

--- src/lib/stores/pagination/paginationTypes.ts
+++ src/lib/stores/pagination/paginationTypes.ts
@@ -7,7 +7,12 @@ export const paginationTypes = [
         }
     },
     {
-        name: 'offsetBased', dynamic: ['offset'], pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => {
+        name: 'offsetBased', dynamic: ['offset'], get_defaultPaginationStateForDynamic: (paginationArgs) => {
+            let offsetName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'offset' })?.dd_displayName
+            let _state = JSON.parse(JSON.stringify(state))
+            _state = { ..._state, [offsetName]: 0 }
+            return _state
+        }, pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => {
             let offsetName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'offset' })?.dd_displayName
             return get(_pagination_state_Store)?.[offsetName] > 0
 
@@ -16,7 +21,12 @@ export const paginationTypes = [
         }
     },
     {
-        name: 'edgeBased', dynamic: ['after', 'before'], stepsOfFieldsToCursor: ['edges', 'cursor'], pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => {
+        name: 'edgeBased', dynamic: ['after', 'before'], get_defaultPaginationStateForDynamic: (state, paginationArgs) => {
+            let afterName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'after' })?.dd_displayName
+            let _state = JSON.parse(JSON.stringify(state))
+            delete _state[afterName]
+            return _state
+        }, stepsOfFieldsToCursor: ['edges', 'cursor'], pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => {
             let afterName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'after' })?.dd_displayName
             let beforeName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'before' })?.dd_displayName
             return get(_pagination_state_Store)?.[afterName] || get(_pagination_state_Store)?.[beforeName]
@@ -25,9 +35,14 @@ export const paginationTypes = [
         }
     },
     {
-        name: 'pageBased', dynamic: ['page'], pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => {
+        name: 'pageBased', dynamic: ['page'], get_defaultPaginationStateForDynamic: (state, paginationArgs) => {
             let pageName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'page' })?.dd_displayName
-            return get(_pagination_state_Store)?.[pageName] > 0
+            let _state = JSON.parse(JSON.stringify(state))
+            _state = { ..._state, [pageName]: 1 }
+            return _state
+        }, pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => {
+            let pageName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'page' })?.dd_displayName
+            return get(_pagination_state_Store)?.[pageName] > 1
         }, checker: (standsForArray) => {
             return standsForArray.includes('page')
         }
