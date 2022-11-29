7eed96deab6ecbd227dabbab418eff8306e7413e
development-vargamarcel <development.vargamarcel@gmail.com>
2022-11-29 11:52:06 +0100

    'generic'

--- src/lib/stores/pagination/paginationTypes.ts
+++ src/lib/stores/pagination/paginationTypes.ts
@@ -2,18 +2,34 @@ import { get } from "svelte/store"
 
 export const paginationTypes = [
     {
-        name: 'notAvailable', pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => { return false }, checker: (standsForArray) => {
+        name: 'notAvailable', get_nextPageState: (state, paginationArgs) => {
+            console.log('notAvailable')
+        }, get_prevPageState: (state, paginationArgs) => {
+            console.log('notAvailable')
+        }, pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => { return false }, checker: (standsForArray) => {
             return standsForArray.length == 0
         }
     },
     {
-        name: 'offsetBased', dynamic: ['offset'], get_defaultPaginationStateForDynamic: (paginationArgs) => {
-            let offsetName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'offset' })?.dd_displayName
-            let _state = JSON.parse(JSON.stringify(state))
-            _state = { ..._state, [offsetName]: 0 }
+        name: 'offsetBased', dynamic: ['offset'], get_defaultPaginationStateForDynamic: (state, paginationArgs) => {
+            const offsetName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'offset' })?.dd_displayName
+            const _state = JSON.parse(JSON.stringify(state))
+            _state[offsetName] = 0
+            return _state
+        }, get_nextPageState: (state, paginationArgs) => {
+            const offsetName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'offset' })?.dd_displayName
+            const limitName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'limit' })?.dd_displayName
+            const _state = JSON.parse(JSON.stringify(state))
+            _state[offsetName] += _state[limitName]
+            return _state
+        }, get_prevPageState: (state, paginationArgs) => {
+            const offsetName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'offset' })?.dd_displayName
+            const limitName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'limit' })?.dd_displayName
+            const _state = JSON.parse(JSON.stringify(state))
+            _state[offsetName] -= _state[limitName]
             return _state
         }, pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => {
-            let offsetName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'offset' })?.dd_displayName
+            const offsetName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'offset' })?.dd_displayName
             return get(_pagination_state_Store)?.[offsetName] > 0
 
         }, checker: (standsForArray) => {
@@ -22,13 +38,15 @@ export const paginationTypes = [
     },
     {
         name: 'edgeBased', dynamic: ['after', 'before'], get_defaultPaginationStateForDynamic: (state, paginationArgs) => {
-            let afterName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'after' })?.dd_displayName
+            const afterName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'after' })?.dd_displayName
+            const beforeName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'before' })?.dd_displayName
             let _state = JSON.parse(JSON.stringify(state))
             delete _state[afterName]
+            delete _state[beforeName]
             return _state
         }, stepsOfFieldsToCursor: ['edges', 'cursor'], pageIsGreaterThenFirst: (_pagination_state_Store, paginationArgs) => {
-            let afterName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'after' })?.dd_displayName
-            let beforeName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'before' })?.dd_displayName
+            const afterName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'after' })?.dd_displayName
+            const beforeName = paginationArgs.find((arg) => { return arg.dd_standsFor == 'before' })?.dd_displayName
             return get(_pagination_state_Store)?.[afterName] || get(_pagination_state_Store)?.[beforeName]
         }, checker: (standsForArray) => {
             return standsForArray.includes('first') || standsForArray.includes('last') && standsForArray.includes('after') || standsForArray.includes('before')
