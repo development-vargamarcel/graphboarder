80715ed2cb8bdc0c812fe127ef5184c918dcbfba
development-vargamarcel <development.vargamarcel@gmail.com>
2022-12-02 09:21:01 +0100

    'generic'

--- src/lib/stores/endpointInfo/endpointInfo.ts
+++ src/lib/stores/endpointInfo/endpointInfo.ts
@@ -1,11 +1,11 @@
 export const endpointInfo = {
-    endpointNames: {
+    ei_Names: {
         hasNextPage: "hasNextPage",
         hasPreviousPage: "hasPreviousPage",
         startCursor: "startCursor",//after,nextPage
         endCursor: "endCursor"//before,previousPage
     },
-    endpointCursorLocations: {
+    ei_CursorLocations: {
         startCursorStepsOfFields: (QMS_name) => { return [QMS_name, 'pageInfo', endpointInfo.endpointNames.startCursor] },
         endCursorStepsOfFields: (QMS_name) => { return [QMS_name, 'pageInfo', endpointInfo.endpointNames.endCursor] },
     }
