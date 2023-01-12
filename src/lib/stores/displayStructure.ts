7cb0ff1770b10b9cafc7dc813812eb4cde8016db
development-vargamarcel <development.vargamarcel@gmail.com>
2023-01-12 12:53:15 +0100

    'generic'

--- src/lib/stores/displayStructure.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from "svelte/store";
-
-export const displayStucture = writable({
-    "timestamptz": "ISO8601",
-    'date_filter_operators': 'ISO8601',
-    'bigint_comparison_exp': 'number',
-    'geometry_comparison_exp': 'geo',
-})
