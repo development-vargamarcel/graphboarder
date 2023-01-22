1e0ea8182e7fde73034a5187ce950782cc0f2195
development-vargamarcel <development.vargamarcel@gmail.com>
2023-01-22 20:05:35 +0100

    'generic'

--- src/lib/stores/pagination/paginationInfo.ts
+++ src/lib/stores/pagination/paginationInfo.ts
@@ -2,17 +2,15 @@ import { writable } from 'svelte/store';
 
 type paginationInfo = {
 	paginationType:
-		| 'offsetBased'
-		| 'edgeBased'
-		| 'idBased'
-		| 'pageBased'
-		| 'notAvailable'
-		| 'unknown';
+	| 'offsetBased'
+	| 'edgeBased'
+	| 'idBased'
+	| 'pageBased'
+	| 'notAvailable'
+	| 'unknown';
 	paginationData: Record<string, unknown>;
 };
-export const Create_paginationInfo = (
-	paginationInfo: paginationInfo = { paginationType: 'offsetBased', paginationData: {} }
-) => {
+export const Create_paginationInfo = (paginationInfo: paginationInfo) => {
 	const store = writable(paginationInfo);
 	const { subscribe, set, update } = store;
 
