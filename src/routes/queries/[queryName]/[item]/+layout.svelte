ba7dd03b83b8c43b25bf76a98432774ae185b2de
development-vargamarcel <development.vargamarcel@gmail.com>
2023-01-31 20:14:43 +0100

    'generic'

--- src/routes/queries/[queryName]/[item]/+layout.svelte
+++ src/routes/queries/[queryName]/[item]/+layout.svelte
@@ -1,16 +1,8 @@
-<script context="module">
-	export const router = true;
-</script>
-
 <script>
-	import { getStores, navigating, page, session, updated } from '$app/stores';
-	import { setClient, getClient, operationStore, query } from '@urql/svelte';
+	import { page } from '$app/stores';
 
-	import { urqlClient } from '$lib/utils/urqlClient';
-	import { onDestroy, onMount } from 'svelte';
 	import Page from '$lib/components/Page.svelte';
 
-	setClient($urqlClient);
 	let item = $page.params.item;
 	//console.log({ item });
 </script>
