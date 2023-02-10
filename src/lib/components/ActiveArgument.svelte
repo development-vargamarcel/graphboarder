1927d46b62d2ed8d027e7492c1b77a8342a3e2d3
development-vargamarcel <58333260+development-vargamarcel@users.noreply.github.com>
2023-02-10 16:09:01 +0100

    Update ActiveArgument.svelte

--- src/lib/components/ActiveArgument.svelte
+++ src/lib/components/ActiveArgument.svelte
@@ -82,7 +82,7 @@
 <label
 	use:clickOutside
 	on:click_outside={handleClickOutside}
-	class=" w-min-[30vw]  rounded-box {expandedVersion
+	class=" w-[40vw]  rounded-box {expandedVersion
 		? 'p-2'
 		: ''}  my-1 flex   dnd-item {activeArgumentData?.inUse
 		? activeArgumentData.canRunQuery
