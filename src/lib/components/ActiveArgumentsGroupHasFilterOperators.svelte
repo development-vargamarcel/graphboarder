11f50c7ddbcdccd860e0d4cd20c5521dd5487744
development-vargamarcel <development.vargamarcel@gmail.com>
2023-02-12 15:30:55 +0100

    'generic'

--- src/lib/components/ActiveArgumentsGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentsGroupHasFilterOperators.svelte
@@ -355,7 +355,7 @@
 	{#if node.hasOwnProperty('items')}
 		<section
 			class=" rounded-l-none {node?.items?.length <= 1 ? 'pt-0' : 'pb-0'} {node?.isMain
-				? ' border-l-2 border-l-transparent  min-h-[30vh] md:min-h-[50vh]'
+				? ' border-l-2 border-l-transparent  min-h-[10vh] md:min-h-[20vh]'
 				: ' '}
 				 w-full"
 			use:dndzone={{
