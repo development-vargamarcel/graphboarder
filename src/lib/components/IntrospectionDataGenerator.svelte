cc523d11fefdda1f1b9422151b16ce18c8fb6749
development-vargamarcel <development.vargamarcel@gmail.com>
2023-02-13 18:33:37 +0100

    'generic'

--- src/lib/components/IntrospectionDataGenerator.svelte
+++ src/lib/components/IntrospectionDataGenerator.svelte
@@ -146,12 +146,61 @@
 			//console.log($queryStore?.data);
 			handleData();
 		} else if ($queryStore?.error) {
-			//console.log($queryStore?.error);
+			console.log($queryStore?.error);
 		} else {
 			//console.log('no data');
 		}
 	}
 </script>
+
+{#if $queryStore?.error}
+	<!-- The button to open modal -->
+	<!-- <label for="my-modal" class="btn">open modal</label> -->
+
+	<!-- Put this part before </body> tag -->
+	<input type="checkbox" checked id="my-modal" class="modal-toggle" />
+	<div class="modal">
+		<div class="modal-box">
+			<div class="alert alert-error shadow-lg">
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="stroke-current flex-shrink-0 h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+						/></svg
+					>
+					<span>{$queryStore?.error}</span>
+				</div>
+			</div>
+			<div class="alert alert-info shadow-lg mt-4">
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="stroke-current flex-shrink-0 w-6 h-6"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+						/></svg
+					>
+					<span>Please check your endpoint.</span>
+				</div>
+			</div>
+			<div class="modal-action">
+				<label for="my-modal" class="btn">close</label>
+			</div>
+		</div>
+	</div>
+{/if}
 
 <style>
 </style>
