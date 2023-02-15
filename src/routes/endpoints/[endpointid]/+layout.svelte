c516b0e3af47b03cf90934163a6734a75ebf9042
development-vargamarcel <development.vargamarcel@gmail.com>
2023-02-15 16:48:48 +0100

    'generic'

--- src/routes/endpoints/[endpointid]/+layout.svelte
+++ src/routes/endpoints/[endpointid]/+layout.svelte
@@ -4,19 +4,17 @@
 	export let data: LayoutData;
 	import { page } from '$app/stores';
 	$: endpointid = $page.params.endpointid;
+	import EndpointsList from '$lib/components/EndpointsList.svelte';
+	import QMSWraper from '$lib/components/QMSWraper.svelte';
 </script>
 
 <div>
 	{endpointid}
 </div>
-<script>
-	import EndpointsList from '$lib/components/EndpointsList.svelte';
-	import QMSWraper from '$lib/components/QMSWraper.svelte';
-</script>
 
-<QMSWraper QMSName="endpoints">
+<QMSWraper QMSName="endpoints_by_id">
 	<div class="pt-2">
-		<EndpointsList QMSName="endpoints" />
+		<EndpointsList QMSName="endpoints_by_id" />
 	</div>
 </QMSWraper>
 <slot><!-- optional fallback --></slot>
