bb7c3929c1af1a13198d05af9f63f4ffdcff4e07
development-vargamarcel <development.vargamarcel@gmail.com>
2023-02-15 17:03:55 +0100

    'generic'

--- src/lib/stores/QMSHandling/QMS_bodyPart_StoreDerived.ts
+++ src/lib/stores/QMSHandling/QMS_bodyPart_StoreDerived.ts
@@ -12,26 +12,22 @@ export const Create_QMS_bodyPart_StoreDerived = (
 	QMS_type = 'query',
 	QMS_name: string,
 	paginationOptions_Store,
-	paginationState_derived_Store,
+	paginationState_derived_Store, initialGqlArgObj = {}
 ) => {
 	return derived(
 		[finalGqlArgObj_Store, tableColsData_Store, paginationState_derived_Store],
 		([$finalGqlArgObj_Store, $tableColsData_Store], set) => {
-			let paginationState
+			let paginationState = {}
 			if (paginationState_derived_Store?.get_value) {
 				paginationState = paginationState_derived_Store?.get_value()
 			}
 			const get_QMS_args = () => {
-				if (paginationState_derived_Store) {
-					const merged = _.merge({},
-						$finalGqlArgObj_Store?.finalGqlArgObj,
-						paginationState
-					);
+				const merged = _.merge({},
+					$finalGqlArgObj_Store?.finalGqlArgObj || {},
+					paginationState, initialGqlArgObj
+				);
+				if (!_.isEmpty(merged)) {
 					return gqlArgObjToString(merged);
-
-				}
-				if ($finalGqlArgObj_Store?.finalGqlArgObj) {
-					return gqlArgObjToString($finalGqlArgObj_Store?.finalGqlArgObj)
 				}
 				return ''
 			}
