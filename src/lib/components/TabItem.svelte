2c5737fa48a4d5c0ef0a2ced554d5f0f339483fd
development-vargamarcel <development.vargamarcel@gmail.com>
2023-02-16 15:45:16 +0100

    'generic'

--- src/lib/components/TabItem.svelte
+++ src/lib/components/TabItem.svelte
@@ -20,12 +20,12 @@
 <a
 	data-sveltekit-prefetch
 	href={url}
-	class="{$page.url.pathname?.split('/')?.[1] === url?.split('/')?.[1]
+	class="{$page.url.pathname.startsWith(url)
 		? ' font-medium scale-110  bg-base-100'
 		: ' opacity-50 '}  px-3 py-2 text-center text-base-content leading-none flex flex-col  mb-2  justify-center  text-xs hover:no-underline focus:no-underline mx-auto border-b-4 border-base-content border-opacity-0 active:border-opacity-100 transform  cursor-pointer  ease-out "
 >
 	<i
-		class="{$page.url.pathname?.split('/')?.[1] === url?.split('/')?.[1] && hasFill
+		class="{$page.url.pathname.startsWith(url) && hasFill
 			? icon + '-fill '
 			: icon + '  '}  text-lg mx-auto leading-none"
 	/>
