edc77325f5661fcd1764d70ae3164c0d219dcd86
development-vargamarcel <development.vargamarcel@gmail.com>
2023-02-24 18:14:34 +0100

    'generic'

--- src/lib/components/ActiveArgumentsGroupHasFilterOperators.svelte
+++ src/lib/components/ActiveArgumentsGroupHasFilterOperators.svelte
@@ -289,7 +289,7 @@
 						: node?.operator == '_and'
 						? 'text-primary'
 						: 'text-accent-focus'} break-all h-max  w-max
-						{node?.not ? ' bg-gradient-to-r from-secondary/100' : 'bg-error/0'}
+						{node?.not ? ' bg-gradient-to-r from-secondary/50' : 'bg-error/0'}
 						"
 					on:click={() => {
 						showModal = true;
