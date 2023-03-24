5a21062b2b34aecd6ec0aa11e48b5fd23bf4a32e
development-vargamarcel <development.vargamarcel@gmail.com>
2023-03-24 11:07:08 +0100

    'generic'

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,1 +1,53 @@
+<script>
+	import { parseAll } from '$lib/stores/testData/testEndpoints';
 
+	let text = '{ "a": 1, "b": 2 }';
+
+	let testObject = {};
+	$: {
+		console.log(text, parseAll(text));
+		testObject = parseAll(text);
+		console.log(testObject);
+		console.log(objectToSourceCode(testObject));
+	}
+
+	function objectToSourceCode(obj) {
+		// Check if the input is an object
+		if (typeof obj !== 'object' || obj === null) {
+			throw new Error('Input must be an object');
+		}
+
+		// Helper function to convert functions to strings
+		function functionToString(fn) {
+			return fn.toString();
+		}
+
+		// Recursively convert the object to source code
+		function convertObjectToSourceCode(obj) {
+			if (typeof obj === 'function') {
+				return functionToString(obj);
+			}
+
+			if (Array.isArray(obj)) {
+				return '[' + obj.map(convertObjectToSourceCode).join(', ') + ']';
+			}
+
+			if (typeof obj === 'object') {
+				return (
+					'{' +
+					Object.entries(obj)
+						.map(([key, value]) => `${key}: ${convertObjectToSourceCode(value)}`)
+						.join(', ') +
+					'}'
+				);
+			}
+
+			// All other types are converted to string literals
+			return JSON.stringify(obj);
+		}
+
+		return convertObjectToSourceCode(obj);
+	}
+</script>
+
+<input type="text" bind:value={text} />
