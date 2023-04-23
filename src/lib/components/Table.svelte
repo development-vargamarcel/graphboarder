1a37357c07715589b5495f8a74bc74155d70ab2a
development-vargamarcel <development.vargamarcel@gmail.com>
2023-04-23 11:02:46 +0200

    'generic'

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -2,13 +2,7 @@
 	export let prefix = '';
 	import { getContext, onMount } from 'svelte';
 	import TanTable from './TanTable.svelte';
-	let QMSMainWraperContext = getContext(`${prefix}QMSMainWraperContext`);
-	let QMSWraperContext = getContext(`${prefix}QMSWraperContext`);
 
-	const endpointInfo = QMSMainWraperContext?.endpointInfo;
-	const schemaData = QMSMainWraperContext?.schemaData;
-	const QMS_info = QMSWraperContext?.QMS_info;
-	let idColName = endpointInfo.get_idField(QMS_info, schemaData)?.dd_displayName;
 	export let enableMultiRowSelectionState = true;
 	export let colsData = [];
 	export let showCheckBox = false;
@@ -25,7 +19,6 @@
 		{prefix}
 		bind:data={rows}
 		bind:cols={colsData}
-		{idColName}
 		on:hideColumn
 		{infiniteHandler}
 		on:rowClicked
