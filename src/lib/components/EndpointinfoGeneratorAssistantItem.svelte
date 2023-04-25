bc888ba2a485be4aa18c87d40640e4df1caf7382
development-vargamarcel <development.vargamarcel@gmail.com>
2023-04-25 09:45:44 +0200

    'generic'

--- src/lib/components/EndpointinfoGeneratorAssistantItem.svelte
+++ src/lib/components/EndpointinfoGeneratorAssistantItem.svelte
@@ -1,12 +1,31 @@
 <script>
+	import { getContext } from 'svelte';
+
 	export let type;
 	export let i;
+	export let prefix = '';
+
+	const QMSWraperContext = getContext(`${prefix}QMSWraperContext`);
+	let QMSMainWraperContext = getContext(`${prefix}QMSMainWraperContext`);
+	const schemaData = QMSMainWraperContext?.schemaData;
 </script>
 
 <tr>
 	<th>{i + 1}</th>
-	<td class="break-all"> {type.dd_displayName}</td>
-	<td>{type.dd_rootName}</td>
+	<td
+		class="cursor-pointer hover:text-primary"
+		on:click={() => {
+			console.log(type);
+		}}
+	>
+		{type.dd_displayName}</td
+	>
+	<td
+		class="cursor-pointer hover:text-primary"
+		on:click={() => {
+			console.log(schemaData.get_rootType(null, type.dd_rootName, schemaData));
+		}}>{type.dd_rootName}</td
+	>
 	<td>{type.dd_kindList_NON_NULL ? '!' : ''}</td>
 	<td>{type.dd_kindList ? 'list' : ''}</td>
 	<td>{type.dd_kindEl_NON_NULL ? '!' : ''}</td>
