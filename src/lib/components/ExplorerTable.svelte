8026aedbbf57157ca20b9dfc0db3e6c7ca221aee
development-vargamarcel <development.vargamarcel@gmail.com>
2023-05-03 18:09:24 +0200

    'generic'

--- src/lib/components/ExplorerTable.svelte
+++ src/lib/components/ExplorerTable.svelte
@@ -98,7 +98,7 @@
 	$: console.log({ table }, '$table', $table);
 </script>
 
-<div class=" h-[80vh] w-[90vw] overscroll-contain	 overflow-y-auto rounded-box pb-32 ">
+<div class=" h-[70vh] w-[90vw] max-w-full overscroll-contain	 overflow-y-auto rounded-box pb-32 ">
 	<table class="table table-compact w-full rounded-none">
 		<thead class="sticky top-0 z-20">
 			{#each $table.getHeaderGroups() as headerGroup}
