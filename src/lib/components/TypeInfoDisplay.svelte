f807e2bb4625dc035be3c9af421108a5ac153236
development-vargamarcel <development.vargamarcel@gmail.com>
2023-12-16 17:43:55 +0100

    'generic'

--- src/lib/components/TypeInfoDisplay.svelte
+++ src/lib/components/TypeInfoDisplay.svelte
@@ -80,7 +80,7 @@
 	<div class="min-w-max  w-full  cursor-pointer    rounded-box flex text-base select-none">
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		{#if !canExpand}
-			<input type="checkbox" class="input input-xs checkbox input-accent  mr-1" name="" id="" />
+			<input type="checkbox" class="input checkbox-xs checkbox input-accent  mr-1" name="" id="" />
 		{/if}
 		<div
 			class="min-w-max  w-full  pr-2 text-md hover:text-primary active:font-black duration-100  "
