a6a99afebeb21fa640bb258b4350b9e65ed3ac7c
development-vargamarcel <development.vargamarcel@gmail.com>
2024-02-05 11:00:20 +0100

    'generic'

--- src/lib/components/GraphqlCodeDisplay.svelte
+++ src/lib/components/GraphqlCodeDisplay.svelte
@@ -15,14 +15,14 @@
 </script>
 
 <div class="mockup-code bg-base text-content my-1 mx-2 px-2 ">
-	<div class="max-h-32 overflow-y-auto">
+	<div class="max-h-[50vh] overflow-y-auto">
 		{#if showNonPrettifiedQMSBody}
 			<code class="px-10">{value}</code>
 		{:else}
 			<code class="language-graphql "
 				>{@html hljs.highlight(format(value), { language: 'graphql' }).value.trim()}</code
 			>
-			<div class="mx-4 mt-2">
+			<div class="mx-4 mt-2 ">
 				<CodeEditor rawValue={value} language="graphql" />
 			</div>
 		{/if}
