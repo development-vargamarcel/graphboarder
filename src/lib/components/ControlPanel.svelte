84ecbafa79b2113c4059d4a169a5297b7c2f0cb7
Marcel <development.vargamarcel@gmail.com>
2024-02-19 15:41:49 +0100

    'generic'

--- src/lib/components/ControlPanel.svelte
+++ src/lib/components/ControlPanel.svelte
@@ -1,4 +1,9 @@
 <script>
+	//1.Select type using Type or AddColumn
+	//2.Create QMSWraperCtx if not existent
+	//3.Select node from QMSWraperCtx.ActiveArgumentsDataGrouped_Store (show list of nodes Object.values(nodes) <show stepsOfFields>)
+
+	//4.Add a way to create nested nodes fast,just by needidng to choose
 </script>
 
 <div class="card w-full bg-primary text-primary-content">
