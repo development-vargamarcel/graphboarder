8145319e8890967f87113e2eb6ca9d37a210b904
development-vargamarcel <development.vargamarcel@gmail.com>
2024-02-29 13:08:18 +0100

    'generic'

--- src/lib/components/ControlPanel.svelte
+++ src/lib/components/ControlPanel.svelte
@@ -1,28 +1,7 @@
 <script>
 	import { getContext, setContext } from 'svelte';
-	import AddColumn from './AddColumn.svelte';
-	///not good
-	//1.Select type using Type or AddColumn
-	//2.Create QMSWraperCtx if not existent
-	//3.Select node from QMSWraperCtx.ActiveArgumentsDataGrouped_Store (show list of nodes Object.values(nodes) <show stepsOfFields>)
-	//4.Add a way to create nested nodes fast,just by needidng to choose
-	///
 
-	///
-	//1.stepsOfFieldsThisAppliesTo will go into activeArgumentsContext
-	//1.5 every node must have knowledge of the steps of fields to the filters trough a activeArgumentsContext
-	//
-	//2.Select node from QMSWraperCtx.ActiveArgumentsDataGrouped_Store (show list of nodes Object.values(nodes) <show stepsOfFields>)
-	///
-	import Type from './Type.svelte';
-	import Modal from './Modal.svelte';
-	import ActiveArguments from './ActiveArguments.svelte';
-	import QMSWraper from './QMSWraper.svelte';
-	import { get } from 'svelte/store';
-	import ActiveArgumentsGroupHasFilterOperators from './ActiveArgumentsGroupHasFilterOperators.svelte';
-	import { each } from 'svelte/internal';
-	import ActiveArgumentsGroupWraper from './ActiveArgumentsGroupWraper.svelte';
-
+	import ControlPanelItem from './ControlPanelItem.svelte';
 	export let type;
 	export let prefix = '';
 	export let column_stepsOfFields;
@@ -31,30 +10,7 @@
 	export let QMSName;
 	export let currentQMS_info;
 	const QMSWraperContext = getContext(`${prefix}OutermostQMSWraperContext`);
-	const {
-		mergedChildren_QMSWraperCtxData_Store,
-		mergedChildren_controlPanel_Store,
-		mergedChildren_finalGqlArgObj_Store
-	} = QMSWraperContext;
-	$: {
-		console.log(
-			'from control panel',
-			QMSWraperContext,
-			$mergedChildren_controlPanel_Store,
-			$mergedChildren_QMSWraperCtxData_Store
-		);
-	}
-
-	const getQMSWraperCtxDataGivenControlPanelItem = (
-		CPItem,
-		mergedChildren_controlPanel_Value,
-		mergedChildren_QMSWraperCtxData_Value
-	) => {
-		const QMSWraperCtxData = mergedChildren_QMSWraperCtxData_Value.find((currCtx) => {
-			return currCtx.stepsOfFields.join() == CPItem.stepsOfFieldsThisAppliesTo.join();
-		});
-		return QMSWraperCtxData;
-	};
+	const { mergedChildren_controlPanel_Store } = QMSWraperContext;
 
 	let showControlPanel = false;
 	const controlPanelContext = {};
@@ -71,23 +27,6 @@
 </button>
 {#if showControlPanel}
 	{#each $mergedChildren_controlPanel_Store as CPItem}
-		{@const activeArgumentsDataGrouped_StoreValCurr = get(
-			getQMSWraperCtxDataGivenControlPanelItem(
-				CPItem,
-				$mergedChildren_controlPanel_Store,
-				$mergedChildren_QMSWraperCtxData_Store
-			).activeArgumentsDataGrouped_Store
-		)}
-		{@const QMSWraperCtxValCurr = getQMSWraperCtxDataGivenControlPanelItem(
-			CPItem,
-			$mergedChildren_controlPanel_Store,
-			$mergedChildren_QMSWraperCtxData_Store
-		)}
-		<ActiveArgumentsGroupWraper
-			on:updateQuery={() => {}}
-			update_activeArgumentsDataGrouped={() => {}}
-			group={activeArgumentsDataGrouped_StoreValCurr[0]}
-			argsInfo={QMSWraperCtxValCurr.QMS_info?.args}
-			{activeArgumentsDataGrouped_StoreValCurr}
-		/>{/each}
+		<ControlPanelItem {CPItem} />
+	{/each}
 {/if}
---
+++ src/lib/components/ControlPanelItem.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { get } from 'svelte/store';
+	import ActiveArgumentsGroupWraper from './ActiveArgumentsGroupWraper.svelte';
+	import { getContext } from 'svelte';
+	export let prefix = '';
+	export let CPItem;
+	const QMSWraperContext = getContext(`${prefix}OutermostQMSWraperContext`);
+	const {
+		mergedChildren_QMSWraperCtxData_Store,
+		mergedChildren_controlPanel_Store,
+		mergedChildren_finalGqlArgObj_Store
+	} = QMSWraperContext;
+
+	const getQMSWraperCtxDataGivenControlPanelItem = (
+		CPItem,
+		mergedChildren_controlPanel_Value,
+		mergedChildren_QMSWraperCtxData_Value
+	) => {
+		const QMSWraperCtxData = mergedChildren_QMSWraperCtxData_Value.find((currCtx) => {
+			return currCtx.stepsOfFields.join() == CPItem.stepsOfFieldsThisAppliesTo.join();
+		});
+		return QMSWraperCtxData;
+	};
+
+	const activeArgumentsDataGrouped_StoreCurr = getQMSWraperCtxDataGivenControlPanelItem(
+		CPItem,
+		$mergedChildren_controlPanel_Store,
+		$mergedChildren_QMSWraperCtxData_Store
+	).activeArgumentsDataGrouped_Store;
+
+	const QMSWraperCtxValCurr = getQMSWraperCtxDataGivenControlPanelItem(
+		CPItem,
+		$mergedChildren_controlPanel_Store,
+		$mergedChildren_QMSWraperCtxData_Store
+	);
+</script>
+
+<ActiveArgumentsGroupWraper
+	on:updateQuery={() => {}}
+	update_activeArgumentsDataGrouped={() => {}}
+	group={$activeArgumentsDataGrouped_StoreCurr[0]}
+	argsInfo={QMSWraperCtxValCurr.QMS_info?.args}
+	activeArgumentsDataGrouped={$activeArgumentsDataGrouped_StoreCurr}
+/>
