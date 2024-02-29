1c10226d9c20673a749eb85ad5d84fed6423ce42
development-vargamarcel <development.vargamarcel@gmail.com>
2024-02-29 12:25:24 +0100

    'generic'

--- src/lib/components/ControlPanel.svelte
+++ src/lib/components/ControlPanel.svelte
@@ -1,6 +1,26 @@
 <script>
 	import { getContext, setContext } from 'svelte';
+	import AddColumn from './AddColumn.svelte';
+	///not good
+	//1.Select type using Type or AddColumn
+	//2.Create QMSWraperCtx if not existent
+	//3.Select node from QMSWraperCtx.ActiveArgumentsDataGrouped_Store (show list of nodes Object.values(nodes) <show stepsOfFields>)
+	//4.Add a way to create nested nodes fast,just by needidng to choose
+	///
+
+	///
+	//1.stepsOfFieldsThisAppliesTo will go into activeArgumentsContext
+	//1.5 every node must have knowledge of the steps of fields to the filters trough a activeArgumentsContext
+	//
+	//2.Select node from QMSWraperCtx.ActiveArgumentsDataGrouped_Store (show list of nodes Object.values(nodes) <show stepsOfFields>)
+	///
+	import Type from './Type.svelte';
+	import Modal from './Modal.svelte';
+	import ActiveArguments from './ActiveArguments.svelte';
+	import QMSWraper from './QMSWraper.svelte';
 	import { get } from 'svelte/store';
+	import ActiveArgumentsGroupHasFilterOperators from './ActiveArgumentsGroupHasFilterOperators.svelte';
+	import { each } from 'svelte/internal';
 	import ActiveArgumentsGroupWraper from './ActiveArgumentsGroupWraper.svelte';
 
 	export let type;
@@ -11,11 +31,23 @@
 	export let QMSName;
 	export let currentQMS_info;
 	const QMSWraperContext = getContext(`${prefix}OutermostQMSWraperContext`);
-	const { mergedChildren_QMSWraperCtxData_Store, mergedChildren_controlPanel_Store } =
-		QMSWraperContext;
+	const {
+		mergedChildren_QMSWraperCtxData_Store,
+		mergedChildren_controlPanel_Store,
+		mergedChildren_finalGqlArgObj_Store
+	} = QMSWraperContext;
+	$: {
+		console.log(
+			'from control panel',
+			QMSWraperContext,
+			$mergedChildren_controlPanel_Store,
+			$mergedChildren_QMSWraperCtxData_Store
+		);
+	}
 
 	const getQMSWraperCtxDataGivenControlPanelItem = (
 		CPItem,
+		mergedChildren_controlPanel_Value,
 		mergedChildren_QMSWraperCtxData_Value
 	) => {
 		const QMSWraperCtxData = mergedChildren_QMSWraperCtxData_Value.find((currCtx) => {
@@ -23,11 +55,20 @@
 		});
 		return QMSWraperCtxData;
 	};
+
 	let showControlPanel = false;
 	const controlPanelContext = {};
 	setContext(`${prefix}controlPanelContext`, controlPanelContext);
 </script>
 
+<!-- <AddColumn
+	bind:column_stepsOfFields
+	{addColumnFromInput}
+	{dd_relatedRoot}
+	{QMSName}
+	QMS_info={currentQMS_info}
+	on:newColumnAddRequest
+/> -->
 <button
 	class="btn btn-xs w-full"
 	on:click={() => {
