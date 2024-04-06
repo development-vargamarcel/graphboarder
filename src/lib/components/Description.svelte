c521a5d26c5a5e7d5ea8d373a1bc35f7fe344a76
development-vargamarcel <development.vargamarcel@gmail.com>
2024-04-06 19:47:34 +0200

    'generic'

--- src/lib/components/Description.svelte
+++ src/lib/components/Description.svelte
@@ -28,9 +28,9 @@
 					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
 				/></svg
 			>
-			<ul class="">
+			<ul class=" space-y-2">
 				{#if nodeRootType?.description}
-					<li class="pb-2">
+					<li class="">
 						{nodeRootType?.description}
 					</li>
 				{/if}
